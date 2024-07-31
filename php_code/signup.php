59e10687e2b0341ee227ed90756cd3c609ef353e
anmol <me@aghadia.com>
2024-07-31 14:52:08 +0530

    added styling to signup page

--- php_code/signup.php
+++ php_code/signup.php
@@ -47,39 +47,191 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My PHP Project</title>
     <style>
-        body { font-family: Arial, sans-serif; margin: 20px; }
-        h1 { color: #333; }
+        /* body { font-family: Arial, sans-serif; margin: 20px; }
+        h1 { color: #333; } */
+
+        @font-face {
+            font-family: 'Handlee';
+            src: url('public/Handlee-Regular.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        * {
+            font-size: 2vh;
+            font-family: Georgia, serif;
+        }
+
+        body, h1, h2, h3, p {
+            margin: 0;
+            padding: 0;
+            font-family:  'Handlee', 'Arial', sans-serif;
+            font-size: 4vh;
+        }
+        
+        @media (orientation: landscape) {
+            body, h1, h2, h3, p {
+                font-size: 3vw;
+            }
+        }
+
+        body {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: linear-gradient(to bottom right, #021526, #03346E, #6EACDA);
+        }
+
+        #signup-container {
+            position: relative;
+            background: white;
+            padding: 40px;
+            border-radius: 8px;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+            width: 70vw;
+            aspect-ratio: 1.15;
+            text-align: center;
+        }
+        
+        @media (orientation: landscape) {
+            #signup-container {
+                width: 45vh;
+                aspect-ratio: 0.85;
+            }
+        }
+
+        form {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+        }
+
+        form label,
+        form div {
+            width: 100%;
+        }
+
+        h2 {
+            margin-bottom: 20px;
+            color: #333;
+        }
+
+        .input-label {
+            text-align: left;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            transition: border-color 0.3s;
+            text-align: center;
+        }
+
+        input:focus {
+            border-color: #6a11cb;
+            outline: none;
+        }
+
+        .notification-container {
+            height: 5vh;
+        }
+
+        .login-container {
+            margin-top: 2.5%; 
+            display: flex;
+            justify-content: center;
+        }
+
+        .login-container i {
+            color: #888;
+            user-select: none;
+        }
+
+        a,
+        a:visited {
+            color: #03346E;
+            margin-left: min(1vh,1vw);
+            transition: color 0.3s;
+        }
+
+        a:hover {
+            color: black;
+            text-decoration: none;
+        }
+
+
+        button {
+            width: 100%;
+            padding: 10px;
+            background: #03346E;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color,color 250ms;
+        }
+
+        button:hover {
+            background: #6EACDA;
+            color: black;
+        }
+
+        .home-page-button {
+            position: absolute;
+            top: 0%;
+            left: 0%;
+            margin: 1vh;
+            background-color: transparent;
+            background-image: url('public/arrow-left-solid.svg');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 100% 100%;
+            width: 3vh;
+            height: 3vh;
+        }
+        
+        .home-page-button:hover {
+            background-size: 90% 90%;
+        }
     </style>
 </head>
 <body>
 
-    <h1>Sign up page</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        Choose Username:
-        <label for="username">
-            <input type="text" name="username" id="username-input" value=<?php echo $username;?>>
-        </label>
-        <br>
-        Enter password:
-        <label for="password">
-            <input type="password" name="password" id="password-input" value=<?php echo $password;?>>
-        </label>
-        <br>
-        Re-enter password:
-        <label for="password-repeat">
-            <input type="password" name="password-repeat" id="password-repeat-input" value=<?php echo $password_repeat;?>>
-        </label>
-        <br>
-        Enter Email:
-        <label for="email">
-            <input type="email" name="email" id="email-input" value=<?php echo $email;?>>
-        </label>
-        <br>
-        <button type="submit">Sign up!</button>
-        <div>Error: <?php echo $error ?></div>
-        <div>Result: <?php echo $result ?></div>
-    </form>
-
+    <div id="signup-container">
+        <h2>Sign up</h2>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label for="username">
+                <div class="input-label">Username</div>
+                <input type="text" name="username" id="username-input" placeholder='Username' value=<?php echo $username;?>>
+            </label>
+            <label for="password">
+                <div class="input-label">Password</div>
+                <input type="password" name="password" id="password-input" placeholder='****' value=<?php echo $password;?>>
+            </label>
+            <label for="password-repeat">
+                <div class="input-label">Confirm Password</div>    
+                <input type="password" name="password-repeat" id="password-repeat-input" placeholder='****' value=<?php echo $password_repeat;?>>
+            </label>
+            <label for="email">
+                <div class="input-label">Email</div>    
+                <input type="email" name="email" id="email-input" placeholder='email@example.com' value=<?php echo $email;?>>
+            </label>
+            <div class='notification-container'>
+                <div><?php echo $error ?></div>
+                <div><?php echo $result ?></div>
+            </div>
+            <button type="submit">Sign up!</button>
+        </form>
+        <div class="login-container"><i>Already have an account?</i><a href="login.php">Login</a></div>
+        <a href="index.php" class='home-page-button'></a>
+    </div>
 
 </body>
 </html>
