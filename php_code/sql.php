4e569c34a361e979e5d710e977f6945da8d8504f
anmol <me@aghadia.com>
2024-11-28 12:47:17 -0800

    example commit for watch tower

--- php_code/sql.php
+++ php_code/sql.php
@@ -1,7 +1,5 @@
 <?php
 $servername = "notes_db";
-// $server_username = file_get_contents('/run/secrets/mysql_username');
-// $server_password = file_get_contents('/run/secrets/mysql_user_password');
 $server_username = 'user';
 $server_password = 'pass';
 $server_database = "notes";
